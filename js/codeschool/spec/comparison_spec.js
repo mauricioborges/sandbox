ae4054d2320d7691168ed7a035495d5874f7e19c
Mauricio Borges Silva <mbsilva@thoughtworks.com>
2015-12-02 12:06:31 -0200

    add null comparision tests

--- js/codeschool/spec/comparison_spec.js
+++ js/codeschool/spec/comparison_spec.js
@@ -8,6 +8,9 @@ describe("what happens when using double equal signs while comparing?", function
    it("treat string '0' as numeric 0", function() {
         expect(0=='0').toBe(true);
    });
+   it("does not treat empty string as equal to null", function() {
+        expect(''==null).toBe(false);
+   });
 });
 describe("what happens when using triple equal signs while comparing?", function(){
    it("does not treat zero as equal to null", function() {
@@ -18,5 +21,8 @@ describe("what happens when using triple equal signs while comparing?", function
    });
    it("does not treat string '0' as a numeric 0", function() {
         expect(0==='0').toBe(false);
+   });
+   it("does not treat empty string as equal to null", function() {
+        expect(''===null).toBe(false);
    });
 });
