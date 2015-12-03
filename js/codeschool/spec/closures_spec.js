b7239d6fcd3b687b6cd0fb4e24ebadb633bd8931
Mauricio Borges Silva <mbsilva@thoughtworks.com>
2015-12-03 17:20:15 -0200

    fix danger zones test to be a real test and created 2 examples about binding time

--- js/codeschool/spec/closures_spec.js
+++ js/codeschool/spec/closures_spec.js
@@ -23,15 +23,46 @@ describe("basicClosure", function(){
         counter.incrementIt();
         expect(counter.acessor()).toBe(1);
     });
+    it("make you aware that bind times are the last ones for closure", function(){
+        var closure = function(arr){
+            var resultFunction;
+            for (var i=0; i < arr.length; i++){
+                if (arr[i] % 2 === 0){
+                    resultFunction = function(){
+                        return i;
+                    };
+                }
+            }
+            return resultFunction;
+        }
+        var firstEvenNumber = closure([1,2,3])();
+        expect(firstEvenNumber).toBe(3);
+        expect(firstEvenNumber).not.toBe(1); //it keeps 'for-ing' to the end
+    });
+    it("and you could break the bind with an eager return", function(){
+        var closure = function(arr){
+            for (var i=0; i < arr.length; i++){
+                if (arr[i] % 2 === 0){
+                    return function(){
+                        return i;
+                    };
+                }
+            }
+        }
+        var firstEvenNumber = closure([1,2,3])();
+        expect(firstEvenNumber).toBe(1);
+        expect(firstEvenNumber).not.toBe(2); 
+    });
 });
 describe("Highway to the Danger Zones", function(){
     var exercise = require("../lib/functions.js");
     it("should display a list of danger zones", function(){
-        var mess=exercise.warningMaker("test");
-        alert=console.log;
-        mess(1,"1");
-        mess(1,"2");
-        fail("this is not a test, where are your assertions?");
+        var messenger=exercise.warningMaker("test");
+        alert = jasmine.createSpy();
+        messenger(1,"1");
+        expect(alert).toHaveBeenCalledWith(jasmine.stringMatching(/.*Current danger zones are:\n1$/));
+        messenger(1,"2");
+        expect(alert).toHaveBeenCalledWith(jasmine.stringMatching(/.*Current danger zones are:\n1\n2$/));
     });
 
 });
