e678cd14e97f12892a081df261666ac2832244b3
Mauricio Borges Silva <mbsilva@thoughtworks.com>
2015-12-09 14:53:19 -0200

    very basic approach to Pomodoro API

--- spring/fmwk/src/main/java/my/pomodoro/Pomodoro.java
+++ spring/fmwk/src/main/java/my/pomodoro/Pomodoro.java
@@ -3,4 +3,16 @@ package my.pomodoro;
 public class Pomodoro {
     public void start() {
     }
+
+    public Boolean isOver() {
+        return Boolean.TRUE;
+    }
+
+    public Boolean isRunning() {
+        return Boolean.TRUE;
+    }
+
+    public Boolean stop() {
+        return Boolean.TRUE;
+    }
 }
--- spring/fmwk/src/test/groovy/basicspec.groovy
+++ spring/fmwk/src/test/groovy/basicspec.groovy
@@ -1,11 +1,31 @@
 import my.pomodoro.Pomodoro
 import spock.lang.Specification
 
-class PomodoroBasic extends Specification {
-    def "should be able to start a pomodoro"(){
+class PomodoroBasicAPI extends Specification {
+    def "should be able to start a pomodoro"() {
         given:
         def pomodoro = new Pomodoro()
         expect:
         pomodoro.start()
+    }
+
+    def "should be able to check if a pomodoro is over"() {
+        given:
+        def pomodoro = new Pomodoro()
+        expect:
+        pomodoro.isOver()
+    }
+
+    def "should be able to check if a pomodoro is running"() {
+        given:
+        def pomodoro = new Pomodoro()
+        expect:
+        pomodoro.isRunning()
+    }
+    def "should be able to stop a pomodoro"() {
+        given:
+        def pomodoro = new Pomodoro()
+        expect:
+        pomodoro.stop()
     }
 }
