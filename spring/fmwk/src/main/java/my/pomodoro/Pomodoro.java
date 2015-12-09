afe512f3a899cce58d197f71625b2063df4729b7
Mauricio Borges Silva <mbsilva@thoughtworks.com>
2015-12-09 15:36:38 -0200

    basic model kind of done

--- spring/fmwk/src/main/java/my/pomodoro/Pomodoro.java
+++ spring/fmwk/src/main/java/my/pomodoro/Pomodoro.java
@@ -1,15 +1,22 @@
 package my.pomodoro;
 
 public class Pomodoro {
+
+    private Boolean running = Boolean.FALSE;
+
     public void start() {
+        running = Boolean.TRUE;
     }
 
     public Boolean isOver() {
+        if (running){
+            return Boolean.FALSE;
+        }
         return Boolean.TRUE;
     }
 
     public Boolean isRunning() {
-        return Boolean.TRUE;
+        return running;
     }
 
     public Boolean stop() {
--- spring/fmwk/src/test/groovy/basicspec.groovy
+++ spring/fmwk/src/test/groovy/basicspec.groovy
@@ -1,7 +1,7 @@
 import my.pomodoro.Pomodoro
 import spock.lang.Specification
 
-class PomodoroBasicAPI extends Specification {
+class PomodoroBasicModel extends Specification {
     def "should be able to start a pomodoro"() {
         given:
         def pomodoro = new Pomodoro()
@@ -16,16 +16,36 @@ class PomodoroBasicAPI extends Specification {
         pomodoro.isOver()
     }
 
-    def "should be able to check if a pomodoro is running"() {
+    def "should mark pomodoro as not over while not stopped"() {
+        given:
+        def pomodoro = new Pomodoro()
+        pomodoro.start()
+        expect:
+        pomodoro.isOver() == false
+    }
+
+
+    def "should show when a pomodoro is running"() {
+        given:
+        def pomodoro = new Pomodoro()
+        when:
+        pomodoro.start()
+        then:
+        pomodoro.isRunning()
+    }
+
+    def "should show when a pomodoro is NOT running"() {
         given:
         def pomodoro = new Pomodoro()
         expect:
-        pomodoro.isRunning()
+        pomodoro.isRunning() == Boolean.FALSE
     }
+
     def "should be able to stop a pomodoro"() {
         given:
         def pomodoro = new Pomodoro()
         expect:
         pomodoro.stop()
     }
+
 }
