d1deba2d488d4676916b22ad104e29512fd68a20
Mauricio Borges Silva <mbsilva@thoughtworks.com>
2015-12-22 18:25:23 -0200

    adds specification to verify behaviour of docker-host package usage

---
+++ protractor/specs/dockerhost-spec.js
@@ -0,0 +1,12 @@
+describe("Docker Host usage:", function(){
+    var dockerHost = require('docker-host');
+    it('should return a remote IP when using an external (or virtualized) docker host', function(){
+        expect(dockerHost('tcp://192.168.99.100:2376').host).toBe('192.168.99.100');
+    });
+    it('should return localhost when using local docker installation', function(){
+        expect(dockerHost('/var/unix/docker/docker.sock').host).toBe('localhost');
+    });
+    it('should presume localhost when none is given', function(){
+        expect(dockerHost('').host).toEqual(jasmine.any(String));
+    });
+});
