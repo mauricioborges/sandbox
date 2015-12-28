f81844c30a952c91005152b22ed2d4cdb4f26c62
Mauricio Borges Silva <mbsilva@thoughtworks.com>
2015-12-28 18:52:30 -0200

    adds more specification on globals

--- protractor/specs/overview_spec.js
+++ protractor/specs/overview_spec.js
@@ -1,5 +1,5 @@
 describe("Working with Spec and Config files:",function(){
-    describe("Global variables:",function(){
+    describe("Protractor globals:",function(){
         describe("browser - an wrapper around an instance of WebDriver:",function(){
             it("should provide 'browser' as a global variable",function(){
                 expect(browser).not.toBe(null);
@@ -11,6 +11,24 @@ describe("Working with Spec and Config files:",function(){
                 expect(browser.driver.session).not.toBe(null);
                 expect(browser.driver.wait).not.toBe(null);
             });
+        });
+        describe("by - a set of action methods (locators) to find items on a page:",function(){
+            it("by should be an Object", function(){
+                expect(by).toEqual(jasmine.any(Object));
+            })
+            it("should be able to access the CSS locator", function(){
+                expect(by.css).toEqual(jasmine.any(Function));
+            })
+        });
+        describe("element - a helper function for finding and interaction with DOM elements:",function(){
+            it("should be able to access the element function", function(){
+                expect(element).toEqual(jasmine.any(Function));
+            })
+        });
+        describe("protractor - a namespace which wraps WebDriver namespace:",function(){
+            it("should be able to access the element function", function(){
+                expect(protractor).toEqual(jasmine.any(Object));
+            })
         });
     });
 });
