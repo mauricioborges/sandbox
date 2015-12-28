aee8158e6098fe42456f0b1a61c3c1ee7f32d8e0
Mauricio Borges Silva <mbsilva@thoughtworks.com>
2015-12-28 18:30:39 -0200

    adds junit reporter

--- protractor/conf.js
+++ protractor/conf.js
@@ -7,9 +7,33 @@ exports.config = {
   },{
     browserName: 'chrome'
   }],
-  onPrepare: function() {
-     var SpecReporter = require('jasmine-spec-reporter');
-     // add jasmine spec reporter
-     jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
-  }
+    onPrepare: function() {
+        var jasmineReporters = require('jasmine-reporters');
+        var SpecReporter = require('jasmine-spec-reporter');
+
+        // returning the promise makes protractor wait for the reporter config before executing tests
+        return browser.getProcessedConfig().then(function(config) {
+            // you could use other properties here if you want, such as platform and version
+            var browserName = config.capabilities.browserName;
+
+            var junitReporter = new jasmineReporters.JUnitXmlReporter({
+                consolidateAll: true,
+                savePath: 'testresults',
+                // this will produce distinct xml files for each capability
+                filePrefix: browserName + '-xmloutput',
+                modifySuiteName: function(generatedSuiteName, suite) {
+                    // this will produce distinct suite names for each capability,
+                    // e.g. 'firefox.login tests' and 'chrome.login tests'
+                    return browserName + '.' + generatedSuiteName;
+                }
+            });
+            jasmine.getEnv().addReporter(junitReporter);
+            jasmine.getEnv().addReporter(new SpecReporter());
+        });
+    },
+    jasmineNodeOpts: {
+        silent: true,
+        showColors: true,
+        print: function(){}
+    }
 }
