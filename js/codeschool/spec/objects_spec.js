6ec1f7e452b47c97b253d28892585575d2223111
Mauricio Borges Silva <mbsilva@thoughtworks.com>
2015-12-29 20:26:34 -0200

    wip: objects examples

---
+++ js/codeschool/spec/objects_spec.js
@@ -0,0 +1,7 @@
+describe("Objects:", function(){
+    describe("Creation", function(){
+        it("should create and empty object",function(){
+            expect({}).toEqual(jasmine.any(Object));
+        });
+    });
+});
