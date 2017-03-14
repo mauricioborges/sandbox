b493e6d9bf24b92d5750566961b59800bc0f6471
Mauricio Borges Silva <mauricioborges@gmail.com>
2017-03-14 14:53:48 -0300

    creates first test

---
+++ spring/udemy/src/test/java/io/github/mauricioborges/udemy/ApplicationContextPlaygroundTest.java
@@ -0,0 +1,24 @@
+package io.github.mauricioborges.udemy;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+
+public class ApplicationContextPlaygroundTest {
+
+    private ApplicationContext context;
+
+    @Before
+    public void setUp() throws Exception {
+        context = new AnnotationConfigApplicationContext(AppConfig.class);
+    }
+
+    @Test
+    public void itDoesntAlwaysReturnTheSameObject() throws Exception {
+        assertEquals(context.getBean(Person.class), context.getBean(Person.class));
+
+    }
+}
