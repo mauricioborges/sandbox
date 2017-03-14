61cec3b4cbb03a14a8145bd287256223aea9dceb
Mauricio Borges Silva <mauricioborges@gmail.com>
2017-03-14 14:54:55 -0300

    in fact, you don't need to configure the bean method when using ComponentScan

--- spring/udemy/src/main/java/io/github/mauricioborges/udemy/AppConfig.java
+++ spring/udemy/src/main/java/io/github/mauricioborges/udemy/AppConfig.java
@@ -1,14 +1,9 @@
 package io.github.mauricioborges.udemy;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class AppConfig {
-    @Bean
-    public Person person() {
-        return new Person();
-    }
 }
