060d1b95c10026ce39a5411b56bf1346a4736a63
Mauricio Borges Silva <mauricioborges@gmail.com>
2017-03-16 11:23:50 -0300

    properties/yaml file

--- spring/udemy-boot/src/main/java/com/example/HomeController.java
+++ spring/udemy-boot/src/main/java/com/example/HomeController.java
@@ -1,13 +1,19 @@
 package com.example;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @Value("${homeController.msg}")//yaml overrides properties file
+    private String homeControllerMsg;
+
+
     @RequestMapping("/")
     public String home() {
-        return "Hello World";
+        return homeControllerMsg;
     }
 }
