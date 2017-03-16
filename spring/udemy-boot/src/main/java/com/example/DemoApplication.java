96e5df022e33cd2d2f36d8e2cba5163152307536
Mauricio Borges Silva <mauricioborges@gmail.com>
2017-03-16 10:40:09 -0300

    adds service

--- spring/udemy-boot/src/main/java/com/example/DemoApplication.java
+++ spring/udemy-boot/src/main/java/com/example/DemoApplication.java
@@ -2,11 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.example", "com.services"})
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+		String[] names = ctx.getBeanDefinitionNames();
+		for (String name: names) {
+			System.out.println(name);
+		}
 	}
 }
---
+++ spring/udemy-boot/src/main/java/com/services/NotificationService.java
@@ -0,0 +1,11 @@
+package com.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+    public void send(){
+
+    }
+}
