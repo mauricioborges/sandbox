0c0bdb184ba0861f829af3faf308ac18f9b72269
Mauricio Borges Silva <mauricioborges@gmail.com>
2017-03-20 14:43:04 -0300

    angular 2 no 2-way data binding

--- angular/tourforheroes/src/app/app.component.ts
+++ angular/tourforheroes/src/app/app.component.ts
@@ -1,7 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input value="{{hero.name}}" placeholder="hero name"/>
+    </div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+//THE ORDER MATTERS!?
+export class AppComponent  { 
+  title = 'Tour of Heroes'; 
+  hero: Hero = { id: 1,
+    name: 'WindStorm'
+  };
+}
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+hero: Hero = { id: 1,
+  name: 'WindStorm'
+};
